3632a643f8602938ba09a9282d77ee19d988fe00
Mladen <mladen.jotanovic@gmail.com>
2016-09-21 13:31:52 +0200

    CRUD za Feed model

---
+++ views/feed/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\FeedSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Feeds';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="feed-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Feed', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'description',
+            'date_posted',
+            'rating',
+            // 'num_views',
+            // 'num_rates',
+            // 'link',
+            // 'image_link',
+            // 'type_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
