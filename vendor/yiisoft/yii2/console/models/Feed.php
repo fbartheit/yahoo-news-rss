e83b67d4a526d3a3f633bb354699eba92a793a67
Mladen <mladen.jotanovic@gmail.com>
2016-09-23 19:15:34 +0200

    Models, folder sa modelima Feed i FeedType

---
+++ vendor/yiisoft/yii2/console/models/Feed.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace yii\console\models;
+/**
+ * This is the model class for table "feed".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property string $date_posted
+ * @property string $link
+ * @property integer $typeId
+ */
+class Feed
+{
+    
+}
---
+++ vendor/yiisoft/yii2/console/models/FeedType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace yii\console\models;
+/**
+ * This is the model class for table "feed_type".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $link
+ *
+ * @property Feed[] $feeds
+ */
+class FeedType 
+{
+   
+}
