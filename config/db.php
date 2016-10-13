965e56ce3d2847105a3b5ad3095dd1476a5050c9
fbartheit <fbartheit@yahoo.com>
2016-10-13 12:29:05 +0200

    - games menu item added, - static legal pages added - links to legal pages provided in the footer

--- config/db.php
+++ config/db.php
@@ -2,11 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-<<<<<<< HEAD
-    'dsn' => 'mysql:host=192.168.0.226;dbname=bravo_projekat',
-=======
     'dsn' => 'mysql:host=192.168.0.252;dbname=bravo_projekat',
->>>>>>> fd0f95487f9cb437aee97fc98f951677a0dc5f1e
     'username' => 'root',
     'password' => 'adminpass',
     'charset' => 'utf8',
